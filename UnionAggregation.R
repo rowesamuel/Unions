5e5947bd41822e9379c2c7edf7836a84a268fa74
rowesamuel <srowe3435@gmail.com>
2018-10-20 14:39:41 -0400

    "Update 10_20_2018"

--- UnionAggregation.R
+++ UnionAggregation.R
@@ -15,6 +15,60 @@ library(leaflet)
 ###############
 #Load CPS
 ###############
-filelocation<-"/Users/Sam/Desktop/UMBC/Proposal/Data/CPS MORG"
-years<-c(2000:2017)
-filenames<-vector(outer(paste0("morg",years,".dta")))
+filelocation<-"/Users/Sam/Desktop/UMBC/Proposal/Data/CPS MORG/"
+years<-c(2016:2017)
+filenames<-paste0(filelocation,"morg",years,".dta")
+cpsvars<-c("intmonth","year","stfips","cbsafips",
+           "class94","lfsr94","unionmme","cmpwgt",
+           "earnwt","dind02","occ2012","race","ethnic",
+           "sex","grade92")
+cps<-do.call(rbind,lapply(filenames,function(x) {
+  tryCatch({
+    print(x)
+    cpsi<-read_dta(x)
+    cpsi<-cpsi[,which(names(cpsi) %in% cpsvars)]
+    },
+    error=function(e){cat("Error: ",conditionMessage(e),"\n",print(x))})
+  }
+))
+
+################
+#Wage & Salary
+################
+#Employed
+cps$emp<-ifelse(cps$lfsr94>=1 & cps$lfsr94<=2,1,0)
+#Self-Employed
+cps$selfemp<-ifelse(cps$class94 >= 6 & cps$class94 <= 8,1,0)
+#Wage&Salary
+cps$w_s<-ifelse(cps$emp==1 & cps$selfemp==0,1,0)
+#Union
+cps$union<-ifelse(cps$unionmme==1,1,0)
+
+#Adjust weights
+#NBER MORG weights are 3 times population
+#http://www.nber.org/data/morg.html
+#Composite Weight
+cps$cmpwgt2<-cps$cmpwgt/3
+#Earnings Weights (for unions)
+cps$earnwt2<-cps$earnwt/(3*4)
+
+################
+#Aggregate
+################
+wage_salary<-aggregate(cps$cmpwgt2,by=list(
+  year=cps$year,
+  wage_salary=cps$w_s),
+  FUN=sum
+)
+union<-aggregate(cps$earnwt2,by=list(
+  year=cps$year,
+  union=cps$union),
+  FUN=sum
+)
+
+union_industry<-aggregate(cps$earnwt2,by=list(
+  year=cps$year,
+  union=cps$union,
+  naics=cps$dind02),
+  FUN=sum
+)
